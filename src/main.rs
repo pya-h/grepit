350ec09aeb0c7f8ea07402182105cd6b6994588d
pya-h <p4ya.gcc@gmail.com>
2026-09-11 01:11:20 +0330

    build!: replace the old name

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-extern crate minigrep;
+extern crate grepit;
 
 use std::process;
 
-use minigrep::config::Config;
+use grepit::config::Config;
 
 fn main() {
     let cmd_args: Vec<String> = std::env::args().collect();
     let config = Config::new(&cmd_args).unwrap_or_else(|err: String| {
-        println!("minigrep failed: {}", err);
+        println!("grepit failed: {}", err);
         process::exit(1);
     });
     
     println!("{}", config.to_string());
 
-    if let Err(err) = minigrep::run(config) {
+    if let Err(err) = grepit::run(config) {
         println!("Process failed: {}", err);
         process::exit(1);
     }
