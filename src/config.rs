5063602123d3ee36a6d7d2b0e679195abf5e6b21
pya-h <p4ya.py@gmail.com>
2026-09-10 15:59:20 +0330

    chore: added the replace_by argument

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,12 @@
 pub struct SearchOptions {
     pub case_sensitive: bool,
     pub by_words: bool,
+    pub replace_by: Option<String>,
 }
 
 impl SearchOptions {
     pub fn defaults() -> SearchOptions {
-        SearchOptions { case_sensitive: true, by_words: false }
+        SearchOptions { case_sensitive: true, by_words: false, replace_by: None }
     }
 }
 pub struct Config<'a> {
@@ -19,16 +20,20 @@ impl<'a> Config<'a> {
         if args.len() < 3 {
             return Err(format!("[Usage] {} <query> <filename>", args[0]));
         }
-        let mut options = SearchOptions {
-            case_sensitive: true,
-            by_words: false,
-        };
-        for i in 3..args.len() {
+        let mut options = SearchOptions::defaults();
+        let args_count = args.len();
+        for i in 3..args_count {
             match args[i].as_str() {
                 "-i" => { options.case_sensitive = false; },
                 "+w" => { options.by_words = true; },
                 "+i" => { options.case_sensitive = true; },
                 "-w" => { options.by_words = false; },
+                "-r" => {
+                    if i == args_count - 1 {
+                        eprintln!("Invalid argument: Replace By option needs a second parameter: the replacement string!");
+                    }
+                    options.replace_by = Some(args[i+1].clone())
+                }
                 _ => { 
                     eprintln!("Invalid argument: {}", args[i]);
                 }
